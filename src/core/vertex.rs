0b776c57b2c6f08ecc54fc3a44d92380fbcfb629
Asi7ho <yann.debain@yahoo.fr>
2024-09-05 18:38:11 +0200

    update triangle to use indices

--- src/core/vertex.rs
+++ src/core/vertex.rs
@@ -29,20 +29,21 @@ impl Vertex {
 }
 
 // Triangle
-// pub const VERTICES: &[Vertex] = &[
-//     Vertex {
-//         position: [0.0, 0.5, 0.0],
-//         color: [1.0, 0.0, 0.0],
-//     },
-//     Vertex {
-//         position: [-0.5, -0.5, 0.0],
-//         color: [0.0, 1.0, 0.0],
-//     },
-//     Vertex {
-//         position: [0.5, -0.5, 0.0],
-//         color: [0.0, 0.0, 1.0],
-//     },
-// ];
+pub const VERTICES: &[Vertex] = &[
+    Vertex {
+        position: [0.0, 0.5, 0.0],
+        color: [1.0, 0.0, 0.0],
+    },
+    Vertex {
+        position: [-0.5, -0.5, 0.0],
+        color: [0.0, 1.0, 0.0],
+    },
+    Vertex {
+        position: [0.5, -0.5, 0.0],
+        color: [0.0, 0.0, 1.0],
+    },
+];
+pub const INDICES: &[u16] = &[0, 1, 2];
 
 // Hexagon
 // pub const VERTICES: &[Vertex] = &[
@@ -116,22 +117,22 @@ impl Vertex {
 // pub const INDICES: &[u16] = &[0, 1, 2, 0, 2, 4, 2, 3, 4];
 
 // Parallelogram
-pub const VERTICES: &[Vertex] = &[
-    Vertex {
-        position: [-0.25, 0.5, 0.0],
-        color: [1.0, 0.0, 0.0],
-    },
-    Vertex {
-        position: [-0.5, -0.5, 0.0],
-        color: [0.5, 0.5, 0.0],
-    },
-    Vertex {
-        position: [0.25, -0.5, 0.0],
-        color: [0.0, 0.5, 0.5],
-    },
-    Vertex {
-        position: [0.5, 0.5, 0.0],
-        color: [0.0, 0.0, 1.0],
-    },
-];
-pub const INDICES: &[u16] = &[0, 1, 2, 0, 2, 3];
+// pub const VERTICES: &[Vertex] = &[
+//     Vertex {
+//         position: [-0.25, 0.5, 0.0],
+//         color: [1.0, 0.0, 0.0],
+//     },
+//     Vertex {
+//         position: [-0.5, -0.5, 0.0],
+//         color: [0.5, 0.5, 0.0],
+//     },
+//     Vertex {
+//         position: [0.25, -0.5, 0.0],
+//         color: [0.0, 0.5, 0.5],
+//     },
+//     Vertex {
+//         position: [0.5, 0.5, 0.0],
+//         color: [0.0, 0.0, 1.0],
+//     },
+// ];
+// pub const INDICES: &[u16] = &[0, 1, 2, 0, 2, 3];
