fbf887f81341c5b304cf7332c2c5f0b4c15e989b
Asi7ho <yann.debain@yahoo.fr>
2024-09-05 18:30:11 +0200

    add parallelogram

--- src/core/vertex.rs
+++ src/core/vertex.rs
@@ -93,6 +93,32 @@ impl Vertex {
 // pub const INDICES: &[u16] = &[0, 1, 3, 1, 2, 3];
 
 // Trapezoid
+// pub const VERTICES: &[Vertex] = &[
+//     Vertex {
+//         position: [-0.25, 0.5, 0.0],
+//         color: [0.5, 0.0, 0.5],
+//     },
+//     Vertex {
+//         position: [-0.5, -0.5, 0.0],
+//         color: [0.5, 0.0, 0.5],
+//     },
+//     Vertex {
+//         position: [0.0, -0.5, 0.0],
+//         color: [0.5, 0.0, 0.5],
+//     },
+//     Vertex {
+//         position: [0.5, -0.5, 0.0],
+//         color: [0.5, 0.0, 0.5],
+//     },
+//     Vertex {
+//         position: [0.25, 0.5, 0.0],
+//         color: [0.5, 0.0, 0.5],
+//     },
+// ];
+
+// pub const INDICES: &[u16] = &[0, 1, 2, 0, 2, 4, 2, 3, 4];
+
+// Parallelogram
 pub const VERTICES: &[Vertex] = &[
     Vertex {
         position: [-0.25, 0.5, 0.0],
@@ -103,17 +129,13 @@ pub const VERTICES: &[Vertex] = &[
         color: [0.5, 0.0, 0.5],
     },
     Vertex {
-        position: [0.0, -0.5, 0.0],
+        position: [0.25, -0.5, 0.0],
         color: [0.5, 0.0, 0.5],
     },
     Vertex {
-        position: [0.5, -0.5, 0.0],
-        color: [0.5, 0.0, 0.5],
-    },
-    Vertex {
-        position: [0.25, 0.5, 0.0],
+        position: [0.5, 0.5, 0.0],
         color: [0.5, 0.0, 0.5],
     },
 ];
 
-pub const INDICES: &[u16] = &[0, 1, 2, 0, 2, 4, 2, 3, 4];
+pub const INDICES: &[u16] = &[0, 1, 2, 0, 2, 3];
