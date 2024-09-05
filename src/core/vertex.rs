4e7beed7865d8ba5abea7ea853adf11de2e2ea49
Asi7ho <yann.debain@yahoo.fr>
2024-09-05 18:37:28 +0200

    update colors for figures

--- src/core/vertex.rs
+++ src/core/vertex.rs
@@ -48,94 +48,90 @@ impl Vertex {
 // pub const VERTICES: &[Vertex] = &[
 //     Vertex {
 //         position: [-0.0868241, 0.49240386, 0.0],
-//         color: [0.5, 0.0, 0.5],
+//         color: [1.0, 0.0, 0.0],
 //     },
 //     Vertex {
 //         position: [-0.49513406, 0.06958647, 0.0],
-//         color: [0.5, 0.0, 0.5],
+//         color: [0.5, 0.5, 0.0],
 //     },
 //     Vertex {
 //         position: [-0.21918549, -0.44939706, 0.0],
-//         color: [0.5, 0.0, 0.5],
+//         color: [0.0, 1.0, 0.0],
 //     },
 //     Vertex {
 //         position: [0.35966998, -0.3473291, 0.0],
-//         color: [0.5, 0.0, 0.5],
+//         color: [0.0, 0.5, 0.5],
 //     },
 //     Vertex {
 //         position: [0.44147372, 0.2347359, 0.0],
-//         color: [0.5, 0.0, 0.5],
+//         color: [0.0, 0.0, 1.0],
 //     },
 // ];
-
 // pub const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4];
 
 // Rectange
 // pub const VERTICES: &[Vertex] = &[
 //     Vertex {
 //         position: [-0.5, 0.25, 0.0],
-//         color: [0.5, 0.0, 0.5],
+//         color: [1.0, 0.0, 0.0],
 //     },
 //     Vertex {
 //         position: [-0.5, -0.25, 0.0],
-//         color: [0.5, 0.0, 0.5],
+//         color: [0.5, 0.5, 0.0],
 //     },
 //     Vertex {
 //         position: [0.5, -0.25, 0.0],
-//         color: [0.5, 0.0, 0.5],
+//         color: [0.0, 0.5, 0.5],
 //     },
 //     Vertex {
 //         position: [0.5, 0.25, 0.0],
-//         color: [0.5, 0.0, 0.5],
+//         color: [0.0, 0.0, 1.0],
 //     },
 // ];
-
 // pub const INDICES: &[u16] = &[0, 1, 3, 1, 2, 3];
 
 // Trapezoid
 // pub const VERTICES: &[Vertex] = &[
 //     Vertex {
 //         position: [-0.25, 0.5, 0.0],
-//         color: [0.5, 0.0, 0.5],
+//         color: [1.0, 0.0, 0.0],
 //     },
 //     Vertex {
 //         position: [-0.5, -0.5, 0.0],
-//         color: [0.5, 0.0, 0.5],
+//         color: [0.5, 0.5, 0.0],
 //     },
 //     Vertex {
 //         position: [0.0, -0.5, 0.0],
-//         color: [0.5, 0.0, 0.5],
+//         color: [0.0, 1.0, 0.0],
 //     },
 //     Vertex {
 //         position: [0.5, -0.5, 0.0],
-//         color: [0.5, 0.0, 0.5],
+//         color: [0.0, 0.5, 0.5],
 //     },
 //     Vertex {
 //         position: [0.25, 0.5, 0.0],
-//         color: [0.5, 0.0, 0.5],
+//         color: [0.0, 0.0, 1.0],
 //     },
 // ];
-
 // pub const INDICES: &[u16] = &[0, 1, 2, 0, 2, 4, 2, 3, 4];
 
 // Parallelogram
 pub const VERTICES: &[Vertex] = &[
     Vertex {
         position: [-0.25, 0.5, 0.0],
-        color: [0.5, 0.0, 0.5],
+        color: [1.0, 0.0, 0.0],
     },
     Vertex {
         position: [-0.5, -0.5, 0.0],
-        color: [0.5, 0.0, 0.5],
+        color: [0.5, 0.5, 0.0],
     },
     Vertex {
         position: [0.25, -0.5, 0.0],
-        color: [0.5, 0.0, 0.5],
+        color: [0.0, 0.5, 0.5],
     },
     Vertex {
         position: [0.5, 0.5, 0.0],
-        color: [0.5, 0.0, 0.5],
+        color: [0.0, 0.0, 1.0],
     },
 ];
-
 pub const INDICES: &[u16] = &[0, 1, 2, 0, 2, 3];
