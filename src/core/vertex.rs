77479ee21b74b24744bbfeb04bca125fbd791e9e
Asi7ho <yann.debain@yahoo.fr>
2024-09-05 18:25:08 +0200

    add trapezoid

--- src/core/vertex.rs
+++ src/core/vertex.rs
@@ -70,24 +70,50 @@ impl Vertex {
 
 // pub const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4];
 
-// Rectange vertices
+// Rectange
+// pub const VERTICES: &[Vertex] = &[
+//     Vertex {
+//         position: [-0.5, 0.25, 0.0],
+//         color: [0.5, 0.0, 0.5],
+//     },
+//     Vertex {
+//         position: [-0.5, -0.25, 0.0],
+//         color: [0.5, 0.0, 0.5],
+//     },
+//     Vertex {
+//         position: [0.5, -0.25, 0.0],
+//         color: [0.5, 0.0, 0.5],
+//     },
+//     Vertex {
+//         position: [0.5, 0.25, 0.0],
+//         color: [0.5, 0.0, 0.5],
+//     },
+// ];
+
+// pub const INDICES: &[u16] = &[0, 1, 3, 1, 2, 3];
+
+// Trapezoid
 pub const VERTICES: &[Vertex] = &[
     Vertex {
-        position: [-0.5, 0.25, 0.0],
+        position: [-0.25, 0.5, 0.0],
         color: [0.5, 0.0, 0.5],
     },
     Vertex {
-        position: [-0.5, -0.25, 0.0],
+        position: [-0.5, -0.5, 0.0],
         color: [0.5, 0.0, 0.5],
     },
     Vertex {
-        position: [0.5, -0.25, 0.0],
+        position: [0.0, -0.5, 0.0],
         color: [0.5, 0.0, 0.5],
     },
     Vertex {
-        position: [0.5, 0.25, 0.0],
+        position: [0.5, -0.5, 0.0],
+        color: [0.5, 0.0, 0.5],
+    },
+    Vertex {
+        position: [0.25, 0.5, 0.0],
         color: [0.5, 0.0, 0.5],
     },
 ];
 
-pub const INDICES: &[u16] = &[0, 1, 3, 1, 2, 3];
+pub const INDICES: &[u16] = &[0, 1, 2, 0, 2, 4, 2, 3, 4];
